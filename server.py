235b6b93dd1186b2bb1e64cb83eedfc3e4b8c0bb
Paulo Silva <engelik@gmail.com>
2016-10-28 21:46:48 +0100

    added remaining templates

--- server.py
+++ server.py
@@ -25,10 +25,9 @@ def showCatalog():
 
 
 # categories CRUD
-@app.route('/category/new')
-@app.route('/catalog/category/new')
+@app.route('/catalog/category/new', methods=['GET', 'POST'])
 def newCategory():
-    return "new category page"
+    return render_template('new_category.html')
 
 
 @app.route('/catalog/<string:category_name>')
@@ -40,39 +39,54 @@ def showCategory(category_name):
     return render_template('404.html')
 
 
-@app.route('/catalog/<string:category_name>/edit')
+@app.route('/catalog/<string:category_name>/edit', methods=['GET', 'POST'])
 def editCategory(category_name):
-    return "edit category page."
+    selected_category = session.query(Category).filter_by(name=category_name).first()
+    if selected_category:
+        return render_template('edit_category.html', category=selected_category)
+    return render_template('404.html')
 
 
-@app.route('/catalog/<string:category_name>/delete')
+@app.route('/catalog/<string:category_name>/delete', methods=['GET', 'POST'])
 def deleteCategory(category_name):
-    return "delete category page."
+    selected_category = session.query(Category).filter_by(name=category_name).first()
+    if selected_category:
+        return render_template('delete_category.html', category=selected_category)
+    return render_template('404.html')
 
 
 # items CRUD
-@app.route('/catalog/<string:category_name>/item/new')
-def newItem(category_name):
-    return "%s> New Item page." % (category_name)
+@app.route('/catalog/item/new', methods=['GET', 'POST'])
+def newItem():
+    categories = session.query(Category).order_by(Category.name).all()
+    return render_template('new_item.html', categories=categories)
 
 
 @app.route('/catalog/<string:category_name>/<string:item_name>')
 def showItem(category_name, item_name):
     selected_category = session.query(Category).filter_by(name=category_name).first()
-    selected_item = session.query(Item).filter_by(name=item_name).order_by(Item.name).first()
+    selected_item = session.query(Item).filter_by(name=item_name).first()
     if selected_category and selected_item:
         return render_template('item.html', category=selected_category, item=selected_item)
     return render_template('404.html')
 
 
-@app.route('/catalog/<string:category_name>/<string:item_name>/edit')
+@app.route('/catalog/<string:category_name>/<string:item_name>/edit', methods=['GET', 'POST'])
 def editItem(category_name, item_name):
-    return "%s>%s - Edit Item page." % (category_name, item_name)
+    categories = session.query(Category).order_by(Category.name).all()
+    selected_item = session.query(Item).filter_by(name=item_name).first()
+    if selected_item:
+        return render_template('edit_item.html', categories=categories, item=selected_item)
+    return render_template('404.html')
 
 
-@app.route('/catalog/<string:category_name>/<string:item_name>/delete')
+@app.route('/catalog/<string:category_name>/<string:item_name>/delete', methods=['GET', 'POST'])
 def deleteItem(category_name, item_name):
-    return "%s>%s -Delete Item page." % (category_name, item_name)
+    selected_category = session.query(Category).filter_by(name=category_name).first()
+    selected_item = session.query(Item).filter_by(name=item_name).first()
+    if selected_category and selected_item:
+        return render_template('delete_item.html', category=selected_category, item=selected_item)
+    return render_template('404.html')
 
 
 # Run app if run as script:
